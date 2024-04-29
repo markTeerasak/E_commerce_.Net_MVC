266054cdc5e6770c756389d69834a5c8b35a89ed
twichai <twichai4869@gmail.com>
2024-04-29 18:14:28 +0700

    fitst watch run

--- obj/Debug/net8.0/E_commerct_.Net_MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/E_commerct_.Net_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_commerct_.Net_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b8125eff402070d083cf239c50a8c1a036d2c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0bb211bc088c7ac2382ee6dee2ebe9810d0b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_commerct_.Net_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_commerct_.Net_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
